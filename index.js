8446e285907564957ce8436e0533a93a03fa4934
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-10 19:48:26 +0800

    update

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 import http from "http";
 import fs from "fs";
+import url from "url";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/news") {
-    res.writeHead(200, { "Content-Type": "application/json" });
+  console.log(req.url, typeof req.url);
 
+  // localhost:3000/news?type=hot&createtAt=thisweek
+  if (req.url.startsWith("/news")) {
+    const parsedUrl = url.parse(req.url, true);
+
+    console.log(parsedUrl.query.type, parsedUrl.query.createdAt);
+
+    if (parsedUrl.query.type === "hot") {
+    }
+
+    const searchValue = "Utaa";
     const news = [{ title: "Utaa" }, { title: "Tugjgrel" }];
-    res.write(JSON.stringify(news));
+
+    const result = news.filter(value => value.title === searchValue);
+
+    for (let i = 0; i < news.length; i++) {
+      if (news[i].title === searchValue) {
+        result.push(news[i]);
+      }
+    }
+
+    res.write(JSON.stringify(result));
     res.end();
   }
+
   if (req.url === "/currency") {
     res.writeHead(200, { "Content-Type": "application/json" });
 
     const currencies = { dollar: 3000, jpy: 200 };
     res.write(JSON.stringify(currencies));
+    res.end();
+  }
+
+  if (req.url === "/image") {
+    res.writeHead(200, { "Content-type": "image/jpeg" });
+
+    res.write(fs.readFileSync("mori.jpg"));
+
     res.end();
   }
 
@@ -24,3 +52,7 @@ const server = http.createServer((req, res) => {
 server.listen(3000);
 
 console.log("server listening on 3000");
+
+// const printAlert = printValue => {
+//   prompt(printAlert);
+// };
