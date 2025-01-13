e65fd1c4f295264535340f50b458a56790fe939f
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-13 18:47:56 +0800

    dasgal

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+// movies.json file uusgene
+// json file iin medeellig chataar ywuuln
+
 import express from "express";
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  //html info page
+  res.send("info");
+});
+
+app.get("/movies", (req, res) => {
+  // odoo garch bui kinonuuniig butsaan array baidlaar butsaan [{}]
+  res.send("news");
+});
+
+app.get("/movies", (req, res) => {
+  // odoo garch bui kinonuunuud deer kino nemn
+  // {} ==> array.push({})
+  res.send("news");
 });
 
 app.listen(port, () => {
