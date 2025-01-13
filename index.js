a6e72fbe2261b9b5773d3b898c21f6c922d5ab48
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-13 18:14:25 +0800

    express js example

--- index.js
+++ index.js
@@ -1,58 +1,11 @@
-import http from "http";
-import fs from "fs";
-import url from "url";
+import express from "express";
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, typeof req.url);
-
-  // localhost:3000/news?type=hot&createtAt=thisweek
-  if (req.url.startsWith("/news")) {
-    const parsedUrl = url.parse(req.url, true);
-
-    console.log(parsedUrl.query.type, parsedUrl.query.createdAt);
-
-    if (parsedUrl.query.type === "hot") {
-    }
-
-    const searchValue = "Utaa";
-    const news = [{ title: "Utaa" }, { title: "Tugjgrel" }];
-
-    const result = news.filter(value => value.title === searchValue);
-
-    for (let i = 0; i < news.length; i++) {
-      if (news[i].title === searchValue) {
-        result.push(news[i]);
-      }
-    }
-
-    res.write(JSON.stringify(result));
-    res.end();
-  }
-
-  if (req.url === "/currency") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-
-    const currencies = { dollar: 3000, jpy: 200 };
-    res.write(JSON.stringify(currencies));
-    res.end();
-  }
-
-  if (req.url === "/image") {
-    res.writeHead(200, { "Content-type": "image/jpeg" });
-
-    res.write(fs.readFileSync("mori.jpg"));
-
-    res.end();
-  }
-
-  res.writeHead(404, "not found");
-  res.end();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(3000);
-
-console.log("server listening on 3000");
-
-// const printAlert = printValue => {
-//   prompt(printAlert);
-// };
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
