cdce477eb86826325815ddd3e72fa79925e76213
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-14 18:05:50 +0800

    remove sumjs add src folder

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-// movies.json file uusgene
-// json file iin medeellig chataar ywuuln
-
 import express from "express";
 import fs from "fs";
-import path from "path";
 
 const app = express();
 const port = 3000;
--- sum.js
+++
@@ -1,7 +0,0 @@
-const sum = () => {
-  console.log("1+2");
-};
-
-const aa = "1";
-
-export { sum, aa };
