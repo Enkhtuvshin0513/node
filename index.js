dca8121308e9001b2576172348285e3594138333
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-14 18:14:06 +0800

    add app.js on src folder

--- index.js
+++ index.js
@@ -1,39 +1,5 @@
-import express from "express";
-import fs from "fs";
+import { app } from "./src/app.js";
 
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  //html info page
-  const html = fs.readFileSync("./index.html");
-
-  res.setHeader("Content-type", "text/html");
-  res.send(html);
-});
-
-app.get("/movies", (req, res) => {
-  const movies = JSON.parse(fs.readFileSync("./movies.json"));
-
-  const { movieTitile } = req.query;
-
-  res.send({ success: true, data: movies });
-});
-
-app.post("/movies", (req, res) => {
-  const movie = req.body;
-
-  const movies = JSON.parse(fs.readFileSync("./movies.json"));
-
-  movies.push(movie);
-
-  fs.writeFileSync("./movies.json", movies);
-
-  res.send({ success: true, message: "movie added" });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(3001, () => {
+  console.log("server listening on 3001");
 });
---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import fs from "fs";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  //html info page
+  const html = fs.readFileSync("./index.html");
+
+  res.setHeader("Content-type", "text/html");
+  res.send(html);
+});
+
+app.get("/movies", (req, res) => {
+  const movies = JSON.parse(fs.readFileSync("./movies.json"));
+
+  const { movieTitile } = req.query;
+
+  res.send({ success: true, data: movies });
+});
+
+app.post("/movies", (req, res) => {
+  const movie = req.body;
+
+  const movies = JSON.parse(fs.readFileSync("./movies.json"));
+
+  movies.push(movie);
+
+  fs.writeFileSync("./movies.json", movies);
+
+  res.send({ success: true, message: "movie added" });
+});
+
+export { app };
