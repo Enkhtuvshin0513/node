633e0eda746d47d93c83995084ba55bd35ee4442
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-14 18:14:25 +0800

    3001 to 3000

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { app } from "./src/app.js";
 
-app.listen(3001, () => {
-  console.log("server listening on 3001");
+app.listen(3000, () => {
+  console.log("server listening on 3000");
 });
